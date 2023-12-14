54998675a25c2b7dafa0aa81f11bb827422a41a7
Hong <hoang.bui49@gmail.com>
2023-12-13 16:32:38 -0800

    Completed IP and leaflet functionality

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-async function getLocation() {
-    const location = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_6ofPlwVqae8mh1YtOZYJsPHNlWo1j&ipAddress=8.8.8.8')
+async function getLocation(ip) {
+    const location = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_6ofPlwVqae8mh1YtOZYJsPHNlWo1j&ipAddress=${ip}`)
     const locationData = await location.json()
-    console.log(locationData.ip)
-    console.log(`${locationData.location.city}, ${locationData.location.region}`)
-    console.log(locationData.timezone)
-    console.log(locationData.isp)
+    document.querySelector('.ipAddress').innerHTML = locationData.ip
+    document.querySelector('.locationInfo').innerHTML = `${locationData.location.city}, ${locationData.location.region}, ${locationData.location.postalCode}`
+    document.querySelector('.timezoneInfo').innerHTML = `UTC${locationData.location.timezone}`
+    document.querySelector('.ISPInfo').innerHTML = locationData.isp
+    
+    let lat = locationData.location.lat
+    let lng = locationData.location.lng
+    
+    var map = L.map('map').setView([lat, lng], 13);
+    var marker = L.marker([lat, lng]).addTo(map);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
 }
 
-getLocation();
-sdfg 
+getLocation('');
+
+document.querySelector('.IPSearch').addEventListener('click', () => {
+    map = map.remove()
+    getLocation(document.querySelector('.searchInput').value) 
+})
+
+
